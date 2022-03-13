ae0ce6694eadbde11a4ffb46ee14f58ff799f981
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-03-13 22:05:00 +0100

    complete wifi section

--- startup.sh
+++ startup.sh
@@ -2,22 +2,30 @@
 loadkeys it       # Load keyboard keymap, eg. loadkeys es, loadkeys us, loadkeys de
 
 # Connect to WIFI
-read -sp "Insert Wi-Fi Password: " wifiPass
-wdevice=$(iwctl device list | grep wlan | awk '{print $1}')
-iwctl station $wdevice scan
-sleep 1
-iwctl station $wdevice get-networks
-echo 'iwctl --passphrase $wifiPass station $wdevice connect "[WIFI_ESSID]"'              # (Optional) Connect to WIFI network. _ip link show_ to know WIFI_INTERFACE.
+WIFI_INTERFACE=$(iwctl device list | grep wlan | awk '{print $1}')
+iwctl station $WIFI_INTERFACE scan
+iwctl station $WIFI_INTERFACE get-networks
+sleep 1.5
+read -p  "Insert SSID Name: " WIFI_ESSID
+read -sp "Insert Wi-Fi Password: " WIFI_KEY
+iwctl --passphrase $WIFI_KEY station $WIFI_INTERFACE connect $WIFI_ESSID                  # (Optional) Connect to WIFI network. _ip link show_ to know WIFI_INTERFACE.
+if [ "$?" = "1" ]; then
+  echo "Impossibile collegarsi a $WIFI_ESSID"
+  exit 1
+fi
 
 curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash     # Download alis scripts                                                 # Alternative download URL with URL shortener
-curl -sLo alis.conf https://raw.githubusercontent.com/MauroSoli/alis/master/alis.conf    #
+curl -sLo alis.conf https://raw.githubusercontent.com/MauroSoli/alis/master/alis.conf    # Download custom alis.conf
+
 #./alis-asciinema.sh      # (Optional) Start asciinema video recording
 #./alis.sh      # (Optional) Start asciinema video recording
 
-#secure boot Installation
-pacman -Sy sbctl
-sbctl create-keys
-sbctl enroll-keys
-sbctl status
+##secure boot Installation
+#pacman -Sy sbctl
+#sbctl create-keys
+#sbctl enroll-keys
+#sbctl status
 # after reboot
-systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=1+7+8 --tpm2-device=auto --wipe-slot=tpm2
+
+# Enroll luks system key to tpm
+#systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=1+7+8 --tpm2-device=auto --wipe-slot=tpm2
