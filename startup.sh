ebe4f5bb4399a8382c7605a6e01ebc4220e4ae61
MauroSoli <74069420+MauroSoli@users.noreply.github.com>
2022-04-02 20:13:55 +0200

    change dell startup.sh

--- startup.sh
+++ startup.sh
@@ -20,7 +20,7 @@ echo -e "\n"
 sleep 3
 
 curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash         # Download alis scripts
-curl -sL https://raw.githubusercontent.com/MauroSoli/alis/master/alis.conf > alis.conf  # Download custom alis.conf
+curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis.conf > alis.conf         # Download custom alis.conf
 
 #./alis-asciinema.sh      # (Optional) Start asciinema video recording
 ./alis.sh      # (Optional) Start asciinema video recording
