5d70bc057172a091c6fb88b36250fbc0c0410b11
MauroSoli <74069420+MauroSoli@users.noreply.github.com>
2022-04-03 00:54:52 +0200

    Append alis.conf

--- startup.sh
+++ startup.sh
@@ -6,8 +6,8 @@ read -p  "Would you like to connect on wi-fi network?(yes/no): " WIFI_SET
 if [ "$WIFI_SET" = "yes" ]; then
   WIFI_INTERFACE=$(iwctl device list | grep wlan | awk '{print $1}')
   iwctl station $WIFI_INTERFACE scan
-  iwctl station $WIFI_INTERFACE get-networks
   sleep 1
+  iwctl station $WIFI_INTERFACE get-networks
   read -p  "Insert SSID Name: " WIFI_ESSID
   read -sp "Insert Wi-Fi Password: " WIFI_KEY
   iwctl --passphrase $WIFI_KEY station $WIFI_INTERFACE connect $WIFI_ESSID                 # (Optional) Connect to WIFI network. _ip link show_ to know WIFI_INTERFACE.
@@ -19,8 +19,8 @@ fi
 echo -e "\n"
 sleep 3
 
-curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash         # Download alis scripts
-curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis.conf > alis.conf         # Download custom alis.conf
+curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash          # Download alis scripts
+curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis.conf >> alis.conf         # Download custom alis.conf
 
 #./alis-asciinema.sh      # (Optional) Start asciinema video recording
 ./alis.sh      # (Optional) Start asciinema video recording
