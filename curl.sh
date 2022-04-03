5083b29d43d560c8ccc50838f3ef8c55a50a1945
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-03 18:13:03 +0200

    add gnome and slit startup script

---
+++ curl.sh
@@ -0,0 +1,27 @@
+# Start the system with latest Arch Linux installation media
+loadkeys it       # Load keyboard keymap, eg. loadkeys es, loadkeys us, loadkeys de
+
+# Connect to WIFI
+read -p  "Would you like to connect on wi-fi network?(yes/no): " WIFI_SET
+if [ "$WIFI_SET" = "yes" ]; then
+  WIFI_INTERFACE=$(iwctl device list | grep wlan | awk '{print $1}')
+  iwctl station $WIFI_INTERFACE scan
+  sleep 1
+  iwctl station $WIFI_INTERFACE get-networks
+  read -p  "Insert SSID Name: " WIFI_ESSID
+  read -sp "Insert Wi-Fi Password: " WIFI_KEY
+  iwctl --passphrase $WIFI_KEY station $WIFI_INTERFACE connect $WIFI_ESSID                 # (Optional) Connect to WIFI network. _ip link show_ to know WIFI_INTERFACE.
+  if [ "$?" = "1" ]; then
+    echo "Impossibile collegarsi a $WIFI_ESSID"
+    exit 1
+  fi
+fi
+echo -e "\n"
+sleep 3
+
+curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash          # Download alis scripts
+curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/startup.sh > startup.sh 
+curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis.conf >> alis.conf         # Download custom alis.conf
+curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis-packages.conf >> alis-packages.conf          # Download custom alis.conf
+
+bash startup.sh
--- startup.sh
+++ startup.sh
@@ -1,37 +1,11 @@
-# Start the system with latest Arch Linux installation media
-loadkeys it       # Load keyboard keymap, eg. loadkeys es, loadkeys us, loadkeys de
-
-# Connect to WIFI
-read -p  "Would you like to connect on wi-fi network?(yes/no): " WIFI_SET
-if [ "$WIFI_SET" = "yes" ]; then
-  WIFI_INTERFACE=$(iwctl device list | grep wlan | awk '{print $1}')
-  iwctl station $WIFI_INTERFACE scan
-  sleep 1
-  iwctl station $WIFI_INTERFACE get-networks
-  read -p  "Insert SSID Name: " WIFI_ESSID
-  read -sp "Insert Wi-Fi Password: " WIFI_KEY
-  iwctl --passphrase $WIFI_KEY station $WIFI_INTERFACE connect $WIFI_ESSID                 # (Optional) Connect to WIFI network. _ip link show_ to know WIFI_INTERFACE.
-  if [ "$?" = "1" ]; then
-    echo "Impossibile collegarsi a $WIFI_ESSID"
-    exit 1
-  fi
-fi
-echo -e "\n"
-sleep 3
-
-curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash          # Download alis scripts
-curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis.conf >> alis.conf         # Download custom alis.conf
-curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis-packages.conf >> alis-packages.conf          # Download custom alis.conf
-
 #./alis-asciinema.sh      # (Optional) Start asciinema video recording
 ./alis.sh      # (Optional) Start asciinema video recording
 
-##secure boot Installation
-#pacman -Sy sbctl
-#sbctl create-keys
-#sbctl enroll-keys
-#sbctl status
-# after reboot
+#change default kernel
+#arch-chroot /mnt "sed -E 's,archlinux\.conf,archlinux-zen.conf,g' -i /boot/loader/loader.conf"
+
+#arch-chroot /mnt 'sbctl create-keys; sbctl enroll-keys; sbctl status; sbctl verify'
+
 
 # Enroll luks system key to tpm
 #systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=1+7+8 --tpm2-device=auto --wipe-slot=tpm2
