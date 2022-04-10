7b27becfa1d6890055b7f6d18fd0d4b8a3029868
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-11 00:43:47 +0200

    change repo arch

--- archRCSPackages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo pacman -Sy subversion kubectl --noconfirm --needed
-sudo pacman -S virtualbox virtualbox-guest-iso --noconfirm --needed
--- archUpgrade.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo pacman -Sy archlinux-keyring --noconfirm
-sudo pacman -Syu
-yay -Syu
-#systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=7+8 --tpm2-device=auto --wipe-slot=tpm2
