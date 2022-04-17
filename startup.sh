e0567070087feae2602af7f208e2ad881b775225
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-17 10:40:02 +0200

    test post setup script

--- startup.sh
+++ startup.sh
@@ -1,28 +1,31 @@
 # Change DNS
 #sed -E 's,nameserver.*,nameserver 8.8.8.8,g' -i /etc/resolv.conf
 
-#Download alis and custom config
+# Download alis and custom config
 curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash                     # Download alis scripts
 curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis.conf > alis.conf                     # Download custom alis.conf
 curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis-packages.conf >> alis-packages.conf  # Download custom alis-packages.conf
 curl -sL https://raw.githubusercontent.com/MauroSoli/alis/dell/alis-commons.sh > alis-commons.sh  
 
-#./alis-asciinema.sh      # (Optional) Start asciinema video recording
-./alis.sh      
+./alis-asciinema.sh      # (Optional) Start asciinema video recording
+./alis.sh                # Start Alis script
 
-#change default kernel
+# Change default kernel
 sed -E "s,archlinux\.conf,archlinux-zen.conf,g" -i /mnt/boot/loader/loader.conf
 
-# change default timeout sudo
+# Change default timeout sudo
 echo -e "\nDefaults timestamp_timeout=240" >> /mnt/etc/sudoers
 
-# add persistent journalctl
+# Add persistent journalctl
 mkdir -p /mnt/var/log/journal
 
-#Download Custom alis inside chroot
+# Download Custom alis inside chroot
 curl -sL https://github.com/MauroSoli/client_config/archive/refs/heads/dell.zip > /mnt/home/linux/dell.zip
 arch-chroot /mnt unzip /home/linux/dell.zip -d /home/linux
 arch-chroot /mnt rm -fv /home/linux/dell.zip
+
+# Post install script
+arch-chroot /mnt -u linux /bin/bash /home/llinux/client_config-dell/archBasePackages.sh
 
 # Enroll secure boot keys
 #arch-chroot /mnt 'sbctl create-keys; sbctl enroll-keys; sbctl status; sbctl verify'
