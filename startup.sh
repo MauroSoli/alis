09d034db07dd546e1f78ad44fd2e954fa1dbd95d
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-04-20 11:47:46 +0200

    fix

--- startup.sh
+++ startup.sh
@@ -29,7 +29,7 @@ arch-chroot /mnt rm -fv /home/linux/dell.zip
 #arch-chroot /mnt /usr/bin/sbctl enroll-keys
 #arch-chroot /mnt /usr/bin/sbctl status
 #arch-chroot /mnt /usr/bin/sbctl sign-all
-#arch-chroot /mnt /usr/bin/sbctl sbctl verify
+#arch-chroot /mnt /usr/bin/sbctl verify
 
 # Enroll luks system key to tpm
 #systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=1+7+8 --tpm2-device=auto --wipe-slot=tpm2
