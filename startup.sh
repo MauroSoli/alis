3e1dcbaec0223038fd8931368e8df8b4d040b273
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-09-18 03:52:46 +0200

    other optimization

--- startup.sh
+++ startup.sh
@@ -27,17 +27,10 @@ chmod +x configs/*.sh
 chmod +x *.sh
 
 # Change DNS
-#sed -E 's,nameserver.*,nameserver 8.8.8.8,g' -i /etc/resolv.conf
-
-# Download alis and custom config
-#curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/download.sh | bash                             # Download alis scripts
-#curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/alis.sh > alis.sh                         # Download alis.sh stable version
-#curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/alis.conf > alis.conf                     # Download custom alis.conf
-#curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/alis-packages.conf >> alis-packages.conf  # Download custom alis-packages.conf
-#curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/alis-commons.sh > alis-commons.sh  
+#sed -E 's,nameserver.*,nameserver 8.8.8.8,g' -i /etc/resolv.conf 
 
 # alis-asciinema.sh      # (Optional) Start asciinema video recording
-#./alis.sh                # Start Alis script
+./alis.sh                # Start Alis script
 
 # Change default kernel and timeout
 sed -E "s,archlinux\.conf,archlinux-zen.conf,g" -i /mnt/boot/loader/loader.conf
@@ -50,9 +43,9 @@ echo -e "\nDefaults timestamp_timeout=240" >> /mnt/etc/sudoers
 mkdir -p /mnt/var/log/journal
 
 # Download Custom alis inside chroot
-#curl -sL https://github.com/MauroSoli/client_config/archive/refs/heads/latitude5300.zip > /mnt/home/linux/latitude5300.zip
-#arch-chroot /mnt unzip /home/linux/latitude5300.zip -d /home/linux
-#arch-chroot /mnt rm -fv /home/linux/latitude5300.zip
+curl -sL https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip > /mnt/home/linux/latitude5300.zip
+arch-chroot /mnt unzip /home/linux/latitude5300.zip -d /home/linux
+arch-chroot /mnt rm -fv /home/linux/latitude5300.zip
 
 # Change passwd root and liunx
 arch-chroot /mnt passwd root
@@ -62,11 +55,11 @@ arch-chroot /mnt passwd linux
 cryptsetup luksChangeKey /dev/nvme0n1p6 -S 0
 
 # Enroll secure boot keys
-arch-chroot /mnt /usr/bin/sbctl create-keys
-arch-chroot /mnt /usr/bin/sbctl enroll-keys
-arch-chroot /mnt /usr/bin/sbctl status
-arch-chroot /mnt /usr/bin/sbctl sign-all
-arch-chroot /mnt /usr/bin/sbctl verify
+#arch-chroot /mnt /usr/bin/sbctl create-keys
+#arch-chroot /mnt /usr/bin/sbctl enroll-keys
+#arch-chroot /mnt /usr/bin/sbctl status
+#arch-chroot /mnt /usr/bin/sbctl sign-all
+#arch-chroot /mnt /usr/bin/sbctl verify
 
 # Enroll luks system key to tpm
 #systemd-cryptenroll /dev/nvme0n1p1 --tpm2-pcrs=1+7+8 --tpm2-device=auto --wipe-slot=tpm2
