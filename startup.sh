0931d089883c117f7c4011785bf61adc4f93a805
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-09-18 18:48:09 +0200

    restore

--- startup.sh
+++ startup.sh
@@ -32,44 +32,43 @@ sysctl -w net.ipv6.conf.all.disable_ipv6=1
 # alis-asciinema.sh      # (Optional) Start asciinema video recording
 ./alis.sh                # Start Alis script
 
-###temp # Change default kernel and timeout
-###temp sed -E "s,archlinux\.conf,archlinux-zen.conf,g" -i /mnt/boot/loader/loader.conf
-###temp sed -E "s,timeout.*,timeout 0,g" -i /mnt/boot/loader/loader.conf
-###temp 
-###temp # Change default timeout sudo
-###temp echo -e "\nDefaults timestamp_timeout=240" >> /mnt/etc/sudoers
-###temp 
-###temp # Add persistent journalctl
-###temp mkdir -p /mnt/var/log/journal
-###temp 
-###temp # Download Custom alis under chroot system
-###temp mkdir -p /mnt/home/linux/appoggio
-###temp curl -sL https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip > /mnt/home/linux/appoggio/latitude5300.zip
-###temp arch-chroot /mnt unzip /home/linux/appoggio/latitude5300.zip -d /home/linux/appoggio
-###temp arch-chroot /mnt rm -fv /home/linux/appoggio/latitude5300.zip
-###temp 
-###temp # Download client_config repo under chroot 
-###temp curl -sL https://github.com/$GITHUB_USER/client_config/archive/refs/heads/$BRANCH.zip > /mnt/home/linux/appoggio/$BRANCH.zip
-###temp arch-chroot /mnt unzip /home/linux/appoggio/$BRANCH.zip -d /home/linux/appoggio
-###temp arch-chroot /mnt rm -fv /home/linux/appoggio/$BRANCH.zip
-###temp 
-###temp # Change passwd root and liunx
-###temp arch-chroot /mnt passwd root
-###temp arch-chroot /mnt passwd linux
-###temp 
-###temp # Change luks password
-###temp cryptsetup luksChangeKey /dev/nvme0n1p6 -S 0
-###temp 
-###temp # Enroll secure boot keys
-###temp # Ancora da testare! (tranne installazione sbctl) 
-###temp arch-chroot /mnt pacman -Sy sbctl --noconfirm --needed
-###temp #arch-chroot /mnt /usr/bin/sbctl create-keys
-###temp #arch-chroot /mnt /usr/bin/sbctl enroll-keys
-###temp #arch-chroot /mnt /usr/bin/sbctl status
-###temp #arch-chroot /mnt /usr/bin/sbctl verify | boot |
-###temp #awk '{print $2}' | while read line; do sbctl sign $line; done
-###temp #arch-chroot /mnt /usr/bin/sbctl verify
-###temp 
-###temp # Enroll luks system key to tpm
-###temp #systemd-cryptenroll /dev/nvme0n1p6 --tpm2-pcrs=1+7+8 --tpm2-device=auto --wipe-slot=tpm2
-###temp 
+# Change default kernel and timeout
+sed -E "s,archlinux\.conf,archlinux-zen.conf,g" -i /mnt/boot/loader/loader.conf
+sed -E "s,timeout.*,timeout 0,g" -i /mnt/boot/loader/loader.conf
+
+# Change default timeout sudo
+echo -e "\nDefaults timestamp_timeout=240" >> /mnt/etc/sudoers
+
+# Add persistent journalctl
+mkdir -p /mnt/var/log/journal
+
+# Download Custom alis under chroot system
+mkdir -p /mnt/home/linux/appoggio
+curl -sL https://github.com/$GITHUB_USER/alis/archive/refs/heads/$BRANCH.zip > /mnt/home/linux/appoggio/latitude5300.zip
+arch-chroot /mnt unzip /home/linux/appoggio/latitude5300.zip -d /home/linux/appoggio
+arch-chroot /mnt rm -fv /home/linux/appoggio/latitude5300.zip
+
+# Download client_config repo under chroot 
+curl -sL https://github.com/$GITHUB_USER/client_config/archive/refs/heads/$BRANCH.zip > /mnt/home/linux/appoggio/$BRANCH.zip
+arch-chroot /mnt unzip /home/linux/appoggio/$BRANCH.zip -d /home/linux/appoggio
+arch-chroot /mnt rm -fv /home/linux/appoggio/$BRANCH.zip
+
+# Change passwd root and liunx
+arch-chroot /mnt passwd root
+arch-chroot /mnt passwd linux
+
+# Change luks password
+cryptsetup luksChangeKey /dev/nvme0n1p6 -S 0
+
+# Enroll secure boot keys
+# Ancora da testare! (tranne installazione sbctl) 
+arch-chroot /mnt pacman -Sy sbctl --noconfirm --needed
+#arch-chroot /mnt /usr/bin/sbctl create-keys
+#arch-chroot /mnt /usr/bin/sbctl enroll-keys
+#arch-chroot /mnt /usr/bin/sbctl status
+#arch-chroot /mnt /usr/bin/sbctl verify | boot |
+#awk '{print $2}' | while read line; do sbctl sign $line; done
+#arch-chroot /mnt /usr/bin/sbctl verify
+
+# Enroll luks system key to tpm
+#systemd-cryptenroll /dev/nvme0n1p6 --tpm2-pcrs=1+7+8 --tpm2-device=auto --wipe-slot=tpm2
