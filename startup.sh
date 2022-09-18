20909496b3986a0282cdf4b6e7fd3d6a4d091d9f
z.mauro.solidoro@guest.rcs.it <mauro.solidoro@guest.rcs.it>
2022-09-18 02:18:29 +0200

    fix

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 #sed -E 's,nameserver.*,nameserver 8.8.8.8,g' -i /etc/resolv.conf
 
 # Download alis and custom config
-curl -sL https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash                             # Download alis scripts
+curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/download.sh | bash                             # Download alis scripts
 #curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/alis.sh > alis.sh                         # Download alis.sh stable version
 #curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/alis.conf > alis.conf                     # Download custom alis.conf
 #curl -sL https://raw.githubusercontent.com/MauroSoli/alis/latitude5300/alis-packages.conf >> alis-packages.conf  # Download custom alis-packages.conf
